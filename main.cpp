0cd203c596ee56191f95adca62c3604d65086f8d
Dominic Pickford <dominic.pickford16@imperia.ac.uk>
2017-03-04 19:10:27 +0000

    update #3 Task1 (b)

--- main.cpp
+++ main.cpp
@@ -1,13 +1,16 @@
 # include <iostream>
+# include <iomanip>
 # include <string>
+# include <algorithm>
+# include "math.h"
+
+
 # include "t1.h"
-
-
-
-
 
 int main(){
     T1_inputs();
+
+    std::cout << "End of program" << std::endl;
     return 0;
 }
 
--- t1.h
+++ t1.h
@@ -1,48 +1,20 @@
+#include "t1.cpp"
 
-double Double_Value_Input(std::string name){
-    std::string input;
-    double value;
-    do {
-        std::cout << "Please enter desired "<< name <<": ";
-        getline (std::cin, input);
-        value = std::stod(input);
-        if (value <= 0.0) {
-            std::cout << name << " must be positive!" << std::endl;
-        }
-     } while (value <= 0.0);
-    return value;
-}
+// Tools 
+void output_array(double A[6][6]);
+double Double_Value_Input(std::string name);
+int Nx_Input();
 
-int Nx_Input(){
-    std::string input;
-    int value;
-    do {
-        std::cout << "Please enter desired Nx: ";
-        getline (std::cin, input);
-        value = std::stoi(input);
-        if (value <= 0.0) {
-            std::cout << "Nx must be positive!" << std::endl;
-        }
-        else if(value % 2 == 0.){
-            std::cout << "Nx must be impair." << std::endl;
-        }
-     } while (value <= 0.0 || value % 2 == 0.);
-    return value;
-}
+void T1_inputs();
 
 // make sure there is a node under the concentrated force
 
-void T1_inputs(){
-    std::cout << "Initialising Elements" << std::endl;
-    double L = Double_Value_Input("L");
-    int Nx = Nx_Input();
-    double A = Double_Value_Input("A");
-    double I = Double_Value_Input("I");
-    double E = Double_Value_Input("E");
+void Fe_generator(double l, double Qx, double Qy);
+void Ke_generator(double A, double E, double I, double l);
 
-    double l = L/Nx;    // number of elements
-    return;
+void Global_Stiffness_Matrix_assembler(){	// needs to take Ke as argument
+
+
+
 }
-
-
 
